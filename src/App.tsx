460e442ed79499b455f1ca516343a05831ca29c6
Vigneswari <vigneshwari.s@wele.in>
2025-12-12 13:44:59 +0530

    changes in mock interview

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { Sidebar } from './components/Sidebar';
-import { Home } from './pages/Home';
-import { Dashboard } from './pages/Dashboard';
-import { LearningHub } from './pages/LearningHub';
-import { CareerCompass } from './pages/CareerCompass';
-import { MockInterviews } from './pages/MockInterviews';
-import { LearningCommunity } from './pages/LearningCommunity';
-import { Courses } from './pages/Courses';
-import { Webinars } from './pages/Webinars';
-import { Mentors } from './pages/Mentors';
-import { SkillShowcase } from './pages/SkillShowcase';
-import { ChatHistory } from './pages/ChatHistory';
-import { WhatIsBest } from './pages/WhatIsBest';
-import { Chat } from './pages/Chat';
-import { AIChat } from './components/AIChat';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Sidebar } from "./components/Sidebar";
+import { Home } from "./pages/Home";
+import { Dashboard } from "./pages/Dashboard";
+import { LearningHub } from "./pages/LearningHub";
+import { CareerCompass } from "./pages/CareerCompass";
+import { MockInterviews } from "./pages/MockInterviews";
+import { LearningCommunity } from "./pages/LearningCommunity";
+import { Courses } from "./pages/Courses";
+import { Webinars } from "./pages/Webinars";
+import { Mentors } from "./pages/Mentors";
+import { SkillShowcase } from "./pages/SkillShowcase";
+import { ChatHistory } from "./pages/ChatHistory";
+import { WhatIsBest } from "./pages/WhatIsBest";
+import { Chat } from "./pages/Chat";
+import { AIChat } from "./components/AIChat";
 
 function App() {
-
   return (
     <HashRouter>
       <div className="flex h-screen overflow-hidden">
@@ -27,7 +26,6 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/learning-hub" element={<LearningHub />} />
           <Route path="/career-compass" element={<CareerCompass />} />
-          <Route path="/mock-interviews" element={<MockInterviews />} />
           <Route path="/learning-community" element={<LearningCommunity />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/webinars" element={<Webinars />} />
@@ -35,6 +33,7 @@ function App() {
           <Route path="/skill-showcase" element={<SkillShowcase />} />
           <Route path="/chat-history" element={<ChatHistory />} />
           <Route path="/what-is-best" element={<WhatIsBest />} />
+          <Route path="/mock-interviews" element={<MockInterviews />} />
         </Routes>
       </div>
     </HashRouter>
